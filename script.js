76937415d00fabc180ebe6b832f34a5ac9e008e9
rajahwu <vmradford@gmail.com>
2022-11-06 08:25:50 -0500

    refactor code

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ callBtn.addEventListener('click', callFunction);
 const executeBtn = document.getElementById('execute_btn');
 executeBtn.addEventListener('click', executeFunction);
 
-
-
-
 function callFunction() {
     const calledFunction = document.createElement('div');
     callStack.appendChild(calledFunction);
